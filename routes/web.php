2f804c95defde5b53a3ae31d33088f6175c95b51
okkyarrohman <okkyzee@gmail.com>
2022-11-15 15:14:23 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -29,7 +29,7 @@ Route::middleware([
     'verified'
 ])->group(function () {
     Route::get('/dashboard', function () {
-        return view('dashboard');
+        return view('user/user-dashboard');
     })->name('dashboard');
 });
 
